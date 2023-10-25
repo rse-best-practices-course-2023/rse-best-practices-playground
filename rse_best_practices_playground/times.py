083a4e071c24a1d2d31060e85ab742cf75c10b30
Dhruv <Dhruvscools@gmail.com>
2023-10-25 14:37:07 +0100

    added docstring for function calculate_fastest_time

--- rse_best_practices_playground/times.py
+++ rse_best_practices_playground/times.py
@@ -12,6 +12,12 @@ my_level = "beginner"  # beginner or pro
 # All students should try first to solve the problem at this level.
 # ===============================================================
 def calculate_fastest_time(time_list):
+    '''
+    Returns the largest time in a list.
+    
+    Keyword arguments:
+    time_list -- a list of times
+    '''
     fastest_time = time_list[0]
     for time in time_list:
         if time > fastest_time:
